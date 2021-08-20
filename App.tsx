412cd4b95bac07f6dbc53643ac561094597cfd63
developerdanx <d.petre@lookiero.com>
2021-08-20 18:49:04 +0200

    🚀 initial setup

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text>React Native SwiftUi Proxy</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
